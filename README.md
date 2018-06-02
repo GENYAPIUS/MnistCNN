077ea3a329875193db7fa3abe06c7b7e30dc7a48
GENYAPIUS <t6589989@gmail.com>
2018-06-02 13:43:23 +0800

    MnistCNN

--- README.md
+++ README.md
@@ -24,5 +24,18 @@ pip insyall h5py
 
 要使用jupyter notebook開啟程式碼，輸入jupyter notebook就會開啟網頁；網頁開啟後點選mnistCNN.ipynb
 
+一格稱為一個Cell，Ctrl+Enter為執行該Cell。
 
+第一個Cell為讀取模組。
+
+第二個Cell為load_data Function，包括圖片維度化以及上label
+圖片路徑為imgs = os.listdir("d:/school/image")這一行。
+因預測範圍只有0~9之手寫數字，檔案名稱格式為 數字.第幾張.jpg 
+例：0.1.jpg為數字0、第1張圖片
+
+第三個Cell為使用load_data Function讀取圖片並將維度標準化。
+
+第四個Cell為在使用完第三個Cell後使用標準化的測驗資料進行預測。
+
+第五個Cell為以混淆矩陣顯示預測結果，橫排predict為預測值、直排label為實際值。
 
--- code sniffer/loadModel.py
+++ code sniffer/loadModel.py
@@ -1,16 +1,10 @@
-import tensorflow as tf
-import numpy as np
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras.utils import np_utils
-
+import numpy as np
 
 # 從 HDF5 檔案中載入模型
 model = load_model("MnistCNN.h5")
 
-# 驗證模型
-score = model.evaluate(x_test, y_test, verbose=0)
-
-# 輸出結果
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+#顯示模型
+print(model.summary())
